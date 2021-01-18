1863e3e371564ae99772434970ee585a6b3ea121
Alexander Bourlotos <alexander.bourlotos@gmail.com>
2021-01-18 11:35:27 -0600

    First Commit

---
+++ src/build/precache-manifest.baf75c2b70f71213f7263a906cb9691f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "e8f62a2727b4cb7dd52f",
+    "url": "/static/css/main.9e0f55c6.chunk.css"
+  },
+  {
+    "revision": "e8f62a2727b4cb7dd52f",
+    "url": "/static/js/main.a3c2c3fa.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "76173f92fa92223b2020",
+    "url": "/static/js/2.e0b0ed77.chunk.js"
+  },
+  {
+    "revision": "40d24cb92e4bb4e0b6705aacdcba44e4",
+    "url": "/index.html"
+  }
+];
